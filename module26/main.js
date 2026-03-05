35d4485a5b0287e4edfca2e4e4a8dbb8cf086e7d
Syari12345 <syari.aliu@gmail.com>
2026-03-05 17:42:05 +0100

    module26

--- module26/main.js
+++ module26/main.js
@@ -1,1 +1,85 @@
-console.log("Hellooooo")
+console.log("Hellooooo");
+
+var x = 10;
+
+var value = "digital school";
+
+var y = 30;
+
+console.log(x+y);
+console.log(x*y);
+console.log(x/y);
+console.log(x-y);
+
+
+
+var a = 99;;
+
+a++;
+
+console.log(a);
+
+
+
+var b = 201;
+
+b--;
+
+console.log(b);
+
+
+
+var c = 500;
+
+c+=5;
+
+console.log(c);
+
+
+
+var d = 300;
+
+d-=5;
+
+console.log(d);
+
+
+
+var e = 580;
+
+e*=5;
+
+console.log(e);
+
+
+
+var m = 580;
+
+m/=5;
+
+console.log(m);
+
+
+
+var v = 100;
+
+var w = 50;
+
+console.log(v == w);
+
+console.log(v != w);
+
+console.log(v < w);
+
+console.log(v > w);
+
+
+
+var emri = "Syari";
+var mbiemri = "Aliu";
+
+var fullName = emri +" "+mbiemri;
+
+console.log(fullName);
+
+console.log(`Hello from ${emri}`);
