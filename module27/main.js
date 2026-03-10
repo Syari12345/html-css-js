0a7670868903cd7047535fb3b77b68a6b27fc2a7
Syari12345 <syari.aliu@gmail.com>
2026-03-10 17:44:37 +0100

    module27

---
+++ module27/main.js
@@ -0,0 +1,12 @@
+function displayAlert(){
+    alert("this text is in the function")
+}
+
+displayAlert()
+
+
+function mbledhja(numri1,numri2){
+    return numri1+numri2;
+}
+
+document.write(mbledhja(5,8));
