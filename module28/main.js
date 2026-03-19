33a77413825eed929a29b25e059ccdf896cae427
Syari12345 <syari.aliu@gmail.com>
2026-03-19 17:46:18 +0100

    module28

--- module28/main.js
+++ module28/main.js
@@ -16,6 +16,7 @@ button3.onmousewheel = function(){
   alert('wheel')
 }
 
+
 var button4 =document.getElementById("btn4");
 
 button4.onmouseleave = function(){
@@ -29,9 +30,31 @@ button5.onmouseover = function(){
   alert('on mouse')
 }
 
-var v_button = document.getElementById("btn6")
 
+var v_button = document.getElementById("btn6")
 var v_text = document.getElementById("txt")
+
 v_button.onclick = function(){
   v_text.style.color='red'
+  v_text.style.backgroundColor='blue'
+  v_text.style.textAlign='center'
+ v_text.style.padding='100px'
+ v_text.style.fontSize='50px'
+}
+
+
+var v_button1 = document.getElementById("btn7")
+var v_text1 = document.getElementById("txt1")
+
+v_button1.onclick = function(){
+
+  v_text1.style.cssText = "color:green; background-color:red; font-size:50px; text-align:center; font-Family: cursive; padding:100px; border: 10px solid; border-radius: 20px;"
+
+}
+
+var ifundit = document.getElementById("ifundit")
+var pg = document.getElementById("pg")
+
+ifundit.onclick = function(){
+  pg.setAttribute("class","paragrafi1")
 }
