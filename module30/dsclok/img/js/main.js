fe27a5f12c8565610ff3ec56637a35076da61ffc
Syari12345 <syari.aliu@gmail.com>
2026-04-14 17:48:38 +0200

    module30

--- module30/dsclok/img/js/main.js
+++ module30/dsclok/img/js/main.js
@@ -21,3 +21,46 @@ function showCurrentTime(){
     clock.innerText = clockTime;
     changeImage();
 };
+
+
+var oneSecond = 1000;
+setInterval(showCurrentTime, oneSecond);
+
+function changeImage(){
+    var time = new Date().getHours();
+
+    var image = "imgg/ds_clock.png"
+    var imageHTMl = document.getElementById("timeimages");
+
+    if(time == wakeuptime){
+        image = "imgg/morning.gif";
+    }
+
+    else if(time == dstime){
+        image = "imgg/class.gif";
+    }
+
+        else if(time == sleeptime){
+            image = "imgg/night.gif"
+        }
+
+        imageHTMl.src == image;
+    
+}
+
+function updateClock(){
+    var wakeUpTimeSelector = document.getElementById("wakeUpTimeSelector");
+wakeuptime = wakeUpTimeSelector.value
+
+var dsTimeSelector = document.getElementById("dsTimeSelector");
+dstime = dsTimeSelector.value;
+
+var sleepTimeSelector = document.getElementById("sleepTimeSelector");
+sleeptime = sleepTimeSelector.value;
+
+}
+
+
+var saveButton = document.getElementById("saveButton");
+saveButton.addEventListener("click", updateClock);
+
