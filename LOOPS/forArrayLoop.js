98711ae5226005a9c4f24f49565b54ca2f26cc70
Syari12345 <syari.aliu@gmail.com>
2026-04-21 17:44:32 +0200

    Loops

---
+++ LOOPS/forArrayLoop.js
@@ -0,0 +1,7 @@
+var names = ["Eren", "Armin", "Mikasa"];
+var x;
+
+for(x of names){
+    document.getElementById("foOfArrayLoop").innerHTML += x + "<br>";
+}
+
---
+++ LOOPS/forinLoops.js
@@ -0,0 +1,12 @@
+var person = {firstName: "John", lastName: "Doe", Age: 25};
+
+var text = "";
+var x;
+
+for(x in person){
+    text +=person[x] + "<br>";
+}
+
+document.getElementById("forInLoop").innerHTML = text;
+console.log(text);
+
