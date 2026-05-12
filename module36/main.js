46e02ab271557f28131805afdf05d9a98cbe7ac4
Syari12345 <syari.aliu@gmail.com>
2026-05-12 17:45:54 +0200

    module36 - jQuery

---
+++ module36/main.js
@@ -0,0 +1,11 @@
+// $(window).ready(function(){
+//     alert("it is working")
+// })
+
+// $("#button").click(function(){
+//     console.log("The testing has been successfully!")
+// })
+
+$("#button").click(function(){
+    $("div").addClass("clicked")
+}). find("span").attr("title", "Hover over me")
